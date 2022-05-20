d8a24aaf595689c021e2373f8f6c5f4a93e95cae
uerdemk <74181716+uerdemk@users.noreply.github.com>
2022-05-20 17:04:02 +0300

    Delete main.bat

--- main.bat
+++
@@ -1,7 +0,0 @@
-docker-compose build
-docker-compose up 
-#timeout /t 10
-#docker cp C:\Users\localuser\AKA_CICD\remote_host jenkins:/tmp/remote-key
-#docker network disconnect remote_host_br0 remote_host
-#docker network connect br0 remote_host
-#pause
